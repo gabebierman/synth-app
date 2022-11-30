93d26d0c7c88d8dfab07bad3d22156177ce9f7d4
gabebierman <gabebierman@gmail.com>
2022-11-29 23:58:42 -0600

    finish about page , create generative page hold

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -1,7 +1,75 @@
 import React from "react";
+import { theme } from "../../shared/themes/Themes";
 
 function About() {
-    return <div>About</div>;
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                textAlign: "center",
+            }}
+        >
+            <h1>About synth-app</h1>
+            <div>
+                This application was build by Gabe Bierman using{" "}
+                <a
+                    style={{ color: theme.palette.primary.main }}
+                    href="https://reactjs.org/"
+                    target="_blank"
+                >
+                    React
+                </a>{" "}
+                and{" "}
+                <a
+                    style={{ color: theme.palette.primary.main }}
+                    href="https://tonejs.github.io/"
+                    target="_blank"
+                >
+                    Tone.js
+                </a>{" "}
+                as my final personal project for the Midland University Code Academy. <br></br>
+                The goal is to explore web-audio-api synthesis using my background in music as
+                the basis for the project. <br></br>
+                While this project has an official due date, I hope to keep maintaining and
+                updating the project in the months to come.
+            </div>
+            <br></br>
+            <h2>Relevant information</h2>
+            <div>
+                GitHub repoistory for this project:{" "}
+                <a
+                    style={{ color: theme.palette.primary.main }}
+                    href="https://github.com/gabebierman/synth-app"
+                    target="_blank"
+                >
+                    synth-app
+                </a>
+            </div>
+            <div>
+                <a
+                    style={{ color: theme.palette.primary.main }}
+                    href="https://github.com/gabebierman"
+                    target="_blank"
+                >
+                    Gabe Bierman GitHub
+                </a>
+            </div>
+            <div>
+                <a
+                    style={{ color: theme.palette.primary.main }}
+                    href="https://www.linkedin.com/in/gabebierman/"
+                    target="_blank"
+                >
+                    Gabe Bierman LinkedIn
+                </a>
+            </div>
+            <br></br>
+            <h2>Changelog</h2>
+            <div>11/29/2022: Initial deployment of project</div>
+        </div>
+    );
 }
 
 export default About;
--- src/components/Pages/Generative.jsx
+++ src/components/Pages/Generative.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { theme } from "../../shared/themes/Themes";
 
 function Generative() {
     return (
-        <div style={{ alignText: "center" }}>
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
             <h1>Generative Synthesis</h1>
             <div>
                 Look's like I ran out of time. Hopefully I can come back and finish this in the
                 future!
+            </div>
+            <div>
+                While you're here!{" "}
+                <a
+                    style={{ color: theme.palette.primary.main }}
+                    href="https://daily.bandcamp.com/lists/generative-music-guide"
+                    target="_blank"
+                >
+                    This post from BandCamp
+                </a>{" "}
+                provides a little bit of information about generative music and includes some
+                wonderful examples.
             </div>
         </div>
     );
